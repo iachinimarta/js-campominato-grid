19aba05a70f0133210711db56168f4745a22a871
Marta Iachini <iachinimarta@gmail.com>
2022-07-13 15:39:02 +0200

    Inseriti i numeri all interno dei box

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click',
     function() {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 1; i <= 100; i++) {
             const container = document.getElementById('container');
             // container.innerHTML = `<div class="box"></div>`;
 
             let newBox = document.createElement("div");
             newBox.classList.add("box");
             container.append(newBox);
+
+            newBox.append([i]);
         }
         
     }
